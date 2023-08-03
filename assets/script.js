eedd477bf5251d66c7856bec1a79d0beaba57598
Olivier Jargoyhen <olivier.jargoyhen@sfr.fr>
2023-08-03 16:15:26 +0200

    étape 2

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,20 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// On récupère les deux flèches
+let next = document.querySelector(".arrow_right");
+let prev = document.querySelector(".arrow_left");
+
+// On met en place les écouteurs d'évènements sur les flèches
+next.addEventListener("click", slideNext);
+prev.addEventListener("click", slidePrev);
+
+// Fonctions
+function slideNext(){
+	console.log("clic à droite");
+}
+
+function slidePrev(){
+	console.log("clic à gauche");
+}
